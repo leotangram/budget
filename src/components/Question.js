f66ddeda5bff1180613b0d096be17d581f7ea18c
Leonardo Omaña <leotangram@gmail.com>
2019-06-10 20:25:44 -0500

    Show message error into question component.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,39 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 function Question() {
+  // Definir el state
+  const [quantity, setQuantity] = useState(0)
+  const [error, setError] = useState(false)
+
+  // Validar el presupuesto
+  const addBudget = e => {
+    e.preventDefault()
+
+    // Validad
+    if (quantity < 1 || isNaN(quantity)) {
+      setError(true)
+      return
+    }
+
+    // Si se pasa la validación
+  }
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
-      <form action=''>
+      {error ? <p className="alert alert-danger error">El presupuesto es incorrecto</p> : null}
+      <form onSubmit={addBudget}>
         <input
           type='number'
           className='u-full-width'
           placeholder='Agrega tu presupuesto'
-          // onChange={}
+          onChange={e => setQuantity(parseInt(e.target.value, 10))}
         />
-        <input type="submit" value="Definir presupuesto" className="button-primary u-full-width"/>
+        <input
+          type='submit'
+          value='Definir presupuesto'
+          className='button-primary u-full-width'
+        />
       </form>
     </Fragment>
   )
